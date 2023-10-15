4825251cde14c8d3e9f6c5ff7c5af874dc92f754
ldamoredev <lauti.damore@gmail.com>
2023-10-15 11:50:01 -0300

    test cache

--- ci/index.js
+++ ci/index.js
@@ -1,16 +1,19 @@
 import { connect } from "@dagger.io/dagger"
 
 connect(async (client) => {
-    const source = client.container()
-        .from("node:16-slim")
-        .withDirectory('/src', client.host().directory('.'), { exclude: ["node_modules/", "ci/"] })
+  const nodeCache = client.cacheVolume("node")
 
-    const runner = source
-        .withWorkdir("/src")
-        .withExec(["npm", "install"])
+  const source = client.container()
+    .from("node:16-slim")
+    .withDirectory('/src', client.host().directory('.'), { exclude: ["node_modules/", "ci/"] })
+    .withMountedCache("/src/node_modules", nodeCache)
 
-    const out = await runner
-        .withExec(["npm", "test", "--", "--watchAll=false"])
-        .stderr()
+  const runner = source
+    .withWorkdir("/src")
+    .withExec(["npm", "install"])
+
+  await runner
+    .withExec(["npm", "test", "--", "--watchAll=false"])
+    .stderr()
 
 }, { LogOutput: process.stdout })
